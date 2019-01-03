8321a275bfa3a7d5a717ddccb5f621040c60cf7b
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-01-03 15:58:35 +0100

    upd float32->float64

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,11 @@ fn token_to_str(token: &Token) -> String {
     format!("word = {}, role = {}", token.word, s)
 }
 
-fn display_all_tokens(tokens: Vec<Token>) {
-    for token in tokens {
-        println!("{}", token_to_str(&token));
-    }
-}
+// fn display_all_tokens(tokens: Vec<Token>) {
+//     for token in tokens {
+//         println!("{}", token_to_str(&token));
+//     }
+// }
 
 fn get_token_role(s: String) -> TokenType {
     if s == "+" || s == "-" {
@@ -52,7 +52,7 @@ fn get_token_role(s: String) -> TokenType {
         } else {
             TokenType::Unknown
         }
-    } else if s.parse::<f32>().is_ok() {
+    } else if s.parse::<f64>().is_ok() {
         TokenType::Coefficient
     } else {
         TokenType::Unknown
@@ -66,9 +66,8 @@ fn main() {
     let mut tokens: Vec<Token> = Vec::new();
 
     for w in words_vec {
-        //let r: TokenType = TokenType::Unknown;
         let r: TokenType = get_token_role(w.to_string());
-        let token = Token {word: w.to_string(), role: r};
+        let token = Token { word: w.to_string(), role: r };
         println!("{}", token_to_str(&token));
         // si le token = unknow ou token = indeterminate mais avec un mauvaise exposant -> lexical erreur
         tokens.push(token);
