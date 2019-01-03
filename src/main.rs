e1ba6c71a0be677129b41aba4cd3623b647527bc
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-01-03 17:51:53 +0100

    add unknown token check and exponent check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process;
+
 #[derive(Debug, PartialEq, Eq)]
 enum TokenType {
     Unknown,
@@ -42,12 +44,6 @@ fn get_token_role(s: String) -> TokenType {
         if s.len() == 1 {
             TokenType::Indeterminate
         } else if s.starts_with("X^") {
-            //let substr = &s[2..];
-            //if substr.parse::<u32>().is_err() {
-                //TokenType::Unknown
-            //} else {
-                //TokenType::Indeterminate
-            //}
             TokenType::Indeterminate
         } else {
             TokenType::Unknown
@@ -94,17 +90,76 @@ fn test_get_token_role() {
     assert_eq!(get_token_role("%".to_string()),                     TokenType::Unknown);
 }
 
+fn check_unknown_token(token: & Token) -> Result<(), String> {
+    if token.role == TokenType::Unknown {
+        Err(format!("Error: {}: Unexpected token", token.word))
+    } else {
+        Ok(())
+    }
+}
+
+fn get_exponent(word: & String) -> Result<u32, String> {
+    if word == "X" {
+        return Ok(1)
+    }
+    let substr = &word[2..];
+    if substr.parse::<u32>().is_err() {
+        Err(format!("Error: {}: Invalid exponent", word))
+    } else {
+        Ok(substr.parse::<u32>().unwrap())
+    }
+}
+
+#[test]
+fn test_get_exponent() {
+    assert_eq!(get_exponent(& "X".to_string()), Ok(1));
+    assert_eq!(get_exponent(& "X^1".to_string()), Ok(1));
+    assert_eq!(get_exponent(& "X^0".to_string()), Ok(0));
+    assert_eq!(get_exponent(& "X^+1".to_string()), Ok(1));
+    assert_eq!(get_exponent(& "X^13".to_string()), Ok(13));
+    assert_eq!(get_exponent(& "X^1352882".to_string()), Ok(1352882));
+
+    assert!(get_exponent(& "X^".to_string()).is_err(), true);
+    assert!(get_exponent(& "X^^".to_string()).is_err(), true);
+    assert!(get_exponent(& "X^X".to_string()).is_err(), true);
+    assert!(get_exponent(& "X^-1".to_string()).is_err(), true);
+    assert!(get_exponent(& "X^++1".to_string()).is_err(), true);
+    assert!(get_exponent(& "X^a1".to_string()).is_err(), true);
+    assert!(get_exponent(& "X^2.5".to_string()).is_err(), true);
+    assert!(get_exponent(& "X^-2.5".to_string()).is_err(), true);
+    assert!(get_exponent(& "X^1/5".to_string()).is_err(), true);
+}
+
+fn exit_with_error(error: String) {
+    eprintln!("{}", error);
+    process::exit(1)
+}
+
 fn main() {
-    //let split = "3 + 2 * X^1 - +4 * X + -2 * X^2".split_whitespace();
-    let split = "3.5678 ++ 2a * X^1 - +4 * X + -02 * X^2".split_whitespace();
+    // let split = "3 + 2 * X^1 - +4 * X + -2 * X^2".split_whitespace();
+    let split = "3 + 2 * X^-1 - +4 * X + -2 * X^2".split_whitespace();
+    // let split = "3.5678 ++ 2a * X^1 - +4 * X + -02 * X^2".split_whitespace();
     let words_vec: Vec<&str> = split.collect();
     let mut tokens: Vec<Token> = Vec::new();
 
     for w in words_vec {
         let r: TokenType = get_token_role(w.to_string());
         let token = Token { word: w.to_string(), role: r };
-        println!("{}", token_to_str(&token));
-        // si le token = unknow ou token = indeterminate mais avec un mauvaise exposant -> lexical erreur
+
+        println!("{}", token_to_str(& token)); // DEBUG
+
+        match check_unknown_token(& token) {
+            Err(e) => exit_with_error(e),
+            Ok(_) => (),
+        }
+        if token.role == TokenType::Indeterminate {
+            match get_exponent(&token.word) {
+                Ok(expo) => println!("exponent = {}", expo), //DEBUG
+                Err(e) => exit_with_error(e),
+            }
+        }
+
+        // si le token = unknow ou token = indeterminate mais avec un mauvaise exponent -> lexical erreur
         tokens.push(token);
     }
     // parsing :
@@ -112,7 +167,7 @@ fn main() {
     // indeterminate = entouré de rien/SeparationOp et rien/FactorOp
     // factorop = entouré de coeff et indeterminate
     // separatorop = entouré de coeff et indeterminate
-    // check exposant du polynome
+    // check exponent du polynome
     // resoudre si tout est ok
     //display_all_tokens(tokens);
 }
