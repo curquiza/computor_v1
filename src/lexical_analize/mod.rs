0f2d31b9f26ccef3007d74c1e94b554cb46f9af3
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-01-11 14:27:06 +0100

    Implement Display for struct Token and enum Type

--- src/lexical_analize/mod.rs
+++ src/lexical_analize/mod.rs
@@ -65,17 +65,17 @@ pub fn tokenize(s: String) -> Result<Vec<token::Token>, String> {
     let split = s.split_whitespace();
     let words_vec: Vec<&str> = split.collect();
     if words_vec.len() == 0 {
-        return Err("No equation found".to_string())
+        return Err("Empty argument".to_string())
     }
 
     let mut tokens: Vec<token::Token> = Vec::new();
     for w in words_vec {
         let r: token::Type = get_token_role(w.to_string());
         let mut token = token::Token { word: w.to_string(), role: r, exponent: 0 };
-        println!("{}", token::to_str(&token)); // DEBUG
         if let Err(e) = handle_lexical_errors(&mut token) {
             return Err(e);
         }
+        println!("{}", token); //DEBUG
         tokens.push(token);
     }
 
